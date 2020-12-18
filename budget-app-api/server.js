8d1793c41a429b2e5ef7bf7aedc02f6bdf48e7ed
Dan Brown <dbrow197@uncc.edu>
2020-12-18 17:50:08 -0500

    migration for heroku pt2

---
+++ budget-app-api/server.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const users = require("./routes/api/users");
+const expenses = require('./routes/expenses');
+const monthlyBudget = require('./routes/monthlyBudgets');
+const app = express();
+var cors = require('cors'); 
+
+
+app.use(cors());
+app.use((request, response, next) => {
+  response.setHeader('Access-Control-Allow-Origin', '*');
+  response.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  response.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  response.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
+app.get('/expense', (request, response) => {
+  response.json(require('./expense.json'));
+});
+
+
+// Bodyparser middleware
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(bodyParser.json());
+
+// DB Config
+const db = require("./config/keys").mongoURI;
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true, useUnifiedTopology: true  },
+    
+  )
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+// Passport config
+require("./config/passport")(passport);
+// Routes
+app.use("/api/users", users);
+app.use('/expenses', expenses);
+app.use('/monthlyBudgets', monthlyBudget)
+
+app.get('/')
+const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
