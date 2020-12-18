127ccf75d5a74761dd2881157979640373270e4e
Dan Brown <dbrow197@uncc.edu>
2020-12-18 18:37:37 -0500

    placed api into main mern app folder

---
+++ budget-app-client/budget-app-api/models/expense.model.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+
+
+const expenseSchema = new Schema({
+    //userID: {type: String, required: true},
+    //username: { type: String, required: true },//ref user model here
+    item: {type: String, required: true},
+    date: {type: Date, required: true},
+    amount: {type: Number, required: true},
+    
+   
+}, {
+    timestamps: true,
+});
+
+const Expense = mongoose.model('Expense', expenseSchema);
+
+module.exports = Expense;
---
+++ budget-app-client/budget-app-api/models/monthlyBudget.model.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+
+//filter by object _ID:
+const monthlyBudgetSchema = new Schema({
+    //username: { type: String, required: true },//ref user model
+    amount: {type: Number, required: true},
+    date: {type: Date, required: true},
+}, {
+    timestamps: true,
+});
+
+const MonthlyBudget = mongoose.model('MonthlyBudget', monthlyBudgetSchema);
+
+module.exports = MonthlyBudget;
