ca2ff8c8b819971999611d9338e3d8dd0d92f4cd
Dan Brown <dbrow197@uncc.edu>
2020-12-18 20:39:12 -0500

    code

--- src/components/layout/Landing.js
+++ src/components/layout/Landing.js
@@ -22,7 +22,7 @@ class Landing extends Component {
                   borderRadius: "3px",
                   letterSpacing: "1.5px"
                 }}
-                className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+                className="btn btn-dark waves-effect waves-light hoverable accent-3"
               >
                 Register
               </Link>
