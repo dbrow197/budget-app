991fe240470ccec69f9d0f8ea307a6b1d94a8f4b
Dan Brown <dbrow197@uncc.edu>
2020-12-18 22:09:37 -0500

    removed devtools from store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,8 +2,6 @@
 import { combineReducers } from "redux";
 import authReducer from './authReducers';
 import errorReducer from './errorReducers';
-//import authReducer from "./authReducer";
-//import errorReducer from "./errorReducer";
 export default combineReducers({
   auth: authReducer,
   errors: errorReducer
