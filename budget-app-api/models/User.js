999b84597f67a22aeac6d92a1f7fdf643bfb1641
Dan Brown <dbrow197@uncc.edu>
2020-12-18 22:52:03 -0500

    restructured api

--- budget-app-api/models/User.js
+++ budget-app-api/models/User.js
@@ -19,4 +19,4 @@ const UserSchema = new Schema({
     default: Date.now
   }
 });
-module.exports = mongoose.model("users", UserSchema);
+module.exports = User = mongoose.model("users", UserSchema);
