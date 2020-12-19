db9b3013dfb76b18ce9c6b80976182873d05fa4a
Dan Brown <dbrow197@uncc.edu>
2020-12-18 19:14:48 -0500

    server.js tweak

--- budget-app-client/budget-app-api/server.js
+++ budget-app-client/budget-app-api/server.js
@@ -52,6 +52,7 @@ app.use("/api/users", users);
 app.use('/expenses', expenses);
 app.use('/monthlyBudgets', monthlyBudget)
 
+
 app.get('/')
-const port = process.env.PORT || 8080; // process.env.port is Heroku's port if you choose to deploy the app there
+const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
